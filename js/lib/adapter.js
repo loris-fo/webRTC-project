fe1038f749d0b5bfc2a75abca6b0712613366f9e
lorisfo <loris-fo@outlook.com>
2015-03-11 14:43:09 +0000

    Set up the node environment and added a bare-bones HTML document

---
+++ js/lib/adapter.js
@@ -0,0 +1,135 @@
+var RTCPeerConnection = null;
+var getUserMedia = null;
+var attachMediaStream = null;
+var reattachMediaStream = null;
+var webrtcDetectedBrowser = null;
+var webrtcDetectedVersion = null;
+
+function trace(text) {
+  if (text[text.length - 1] == '\n') {
+    text = text.substring(0, text.length - 1);
+  }
+  console.log((performance.now() / 1000).toFixed(3) + ": " + text);
+}
+
+if (navigator.mozGetUserMedia) {
+  console.log("This appears to be Firefox");
+
+  webrtcDetectedBrowser = "firefox";
+
+  webrtcDetectedVersion =
+                  parseInt(navigator.userAgent.match(/Firefox\/([0-9]+)\./)[1]);
+
+  RTCPeerConnection = mozRTCPeerConnection;
+
+  RTCSessionDescription = mozRTCSessionDescription;
+
+  RTCIceCandidate = mozRTCIceCandidate;
+
+
+  getUserMedia = navigator.mozGetUserMedia.bind(navigator);
+
+  createIceServer = function(url, username, password) {
+    var iceServer = null;
+    var url_parts = url.split(':');
+    if (url_parts[0].indexOf('stun') === 0) {
+      iceServer = { 'url': url };
+    } else if (url_parts[0].indexOf('turn') === 0 &&
+               (url.indexOf('transport=udp') !== -1 ||
+                url.indexOf('?transport') === -1)) {
+
+      var turn_url_parts = url.split("?");
+      iceServer = { 'url': turn_url_parts[0],
+                    'credential': password,
+                    'username': username };
+    }
+    return iceServer;
+  };
+
+  attachMediaStream = function(element, stream) {
+    console.log("Attaching media stream");
+    element.mozSrcObject = stream;
+    element.play();
+  };
+
+  reattachMediaStream = function(to, from) {
+    console.log("Reattaching media stream");
+    to.mozSrcObject = from.mozSrcObject;
+    to.play();
+  };
+
+  MediaStream.prototype.getVideoTracks = function() {
+    return [];
+  };
+
+  MediaStream.prototype.getAudioTracks = function() {
+    return [];
+  };
+} else if (navigator.webkitGetUserMedia) {
+  console.log("This appears to be Chrome");
+
+  webrtcDetectedBrowser = "chrome";
+  webrtcDetectedVersion =
+             parseInt(navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./)[2]);
+
+  createIceServer = function(url, username, password) {
+    var iceServer = null;
+    var url_parts = url.split(':');
+    if (url_parts[0].indexOf('stun') === 0) {
+      iceServer = { 'url': url };
+    } else if (url_parts[0].indexOf('turn') === 0) {
+      if (webrtcDetectedVersion < 28) {
+        var url_turn_parts = url.split("turn:");
+        iceServer = { 'url': 'turn:' + username + '@' + url_turn_parts[1],
+                      'credential': password };
+      } else {
+        iceServer = { 'url': url,
+                      'credential': password,
+                      'username': username };
+      }
+    }
+    return iceServer;
+  };
+
+  RTCPeerConnection = webkitRTCPeerConnection;
+
+
+  getUserMedia = navigator.webkitGetUserMedia.bind(navigator);
+
+  attachMediaStream = function(element, stream) {
+    if (typeof element.srcObject !== 'undefined') {
+      element.srcObject = stream;
+    } else if (typeof element.mozSrcObject !== 'undefined') {
+      element.mozSrcObject = stream;
+    } else if (typeof element.src !== 'undefined') {
+      element.src = URL.createObjectURL(stream);
+    } else {
+      console.log('Error attaching stream to element.');
+    }
+  };
+
+  reattachMediaStream = function(to, from) {
+    to.src = from.src;
+  };
+
+
+  if (!webkitMediaStream.prototype.getVideoTracks) {
+    webkitMediaStream.prototype.getVideoTracks = function() {
+      return this.videoTracks;
+    };
+    webkitMediaStream.prototype.getAudioTracks = function() {
+      return this.audioTracks;
+    };
+  }
+
+  if (!webkitRTCPeerConnection.prototype.getLocalStreams) {
+    webkitRTCPeerConnection.prototype.getLocalStreams = function() {
+      return this.localStreams;
+    };
+    webkitRTCPeerConnection.prototype.getRemoteStreams = function() {
+      return this.remoteStreams;
+    };
+  }
+} else {
+  console.log("Browser does not appear to be WebRTC-capable");
+}
